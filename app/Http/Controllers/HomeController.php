a399854ede324ec8da6fb11249ec0a7e0177e21c
andrecnl <andrecnl@gitlab.com>
2023-01-10 20:19:46 -0300

    primeiro commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function principal(){
+        return view('index');
+    }
+
+
+    public function contato(){
+        return view('contato');
+    }
+}
